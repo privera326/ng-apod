d969c85a2a6861bc86e12abad2dc9e1143bc27d3
pmatsuda <pat.matsuda@comcast.net>
2019-10-30 21:26:12 -0500

    Add APOD routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//import the component to which you want to redirect.
+import { ApodComponent } from './apod/apod.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //redirect an empty route to a given path
+  { path: '', redirectTo: '/apod', pathMatch: 'full'},
+  //Define the path that will load a given component
+  { path: 'apod', component: ApodComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
