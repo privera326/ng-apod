c06cbd8cd3f678936ba611fe1c2d5e0c000f7130
pmatsuda <pat.matsuda@comcast.net>
2019-11-05 15:48:57 -0600

    Display images and text descriptions

--- src/app/apod/apod.component.ts
+++ src/app/apod/apod.component.ts
@@ -12,7 +12,9 @@ import { Apod } from '../models/apod';
 export class ApodComponent implements OnInit {
 
   apod:Apod;
-
+  //1. Create date as an instance variable
+  date:string;
+  
   constructor(
     private apodService: ApodService,
     //2. Inject ActivatedRoute into the constructor
@@ -29,15 +31,28 @@ export class ApodComponent implements OnInit {
   //4. Replace the current date with an updated method signature
   getApod(date:string): void{
 
+    //If the date is falsy, use today's date
+    if(!date){
+      date = new Date().toISOString().slice(0,10);
+    }
+  
     this.apodService.getApod(date).subscribe(
       (response:any)=>{
         this.apod = response;
-
-        //5. Log the results to the JS console
-        console.log(response);
+        this.date = this.randomDate(new Date(1995,5,16), new Date());
+        console.log(this.apod);
       }
     );
+  
+  }
+  //2. Create a method that returns a random date
+  randomDate(start, end): string{
+    let date = new Date(
+      start.getTime() + Math.random() *
+        (end.getTime() - start.getTime())
+    );
 
+    return new Date(date).toISOString().slice(0,10);
   }
 
 }
